5d7170ddb155b255b2051adddec0f34f9c3724ff
PWidla <patrykwidlazb@gmail.com>
2022-09-07 17:53:57 +0200

    Dodaj pliki projektów.

---
+++ FootballClubWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballClubWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
