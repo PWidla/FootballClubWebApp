9dd54579f7129cf44463fbac639ed64f2f4c4270
PWidla <patrykwidlazb@gmail.com>
2022-09-18 17:41:31 +0200

    Add history view

--- FootballClubWeb/Controllers/HomeController.cs
+++ FootballClubWeb/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace FootballClubWeb.Controllers
             return View();
         }
 
+        public IActionResult History()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
